d99b43892b859985343cb69d07fc067d3228d1dd
erhan bulut <90501327+bulutbilisimbusiness@users.noreply.github.com>
2024-05-13 15:02:00 +0300

    finish

---
+++ src/components/product-detail/description-view.vue
@@ -0,0 +1,11 @@
+<template>
+	<h3>descriptions</h3>
+</template>
+
+<script>
+	export default {
+		name: "de",
+	};
+</script>
+
+<style></style>
---
+++ src/views/ProductView.vue
@@ -0,0 +1,9 @@
+<template>
+	<ProductListComponent />
+</template>
+<script>
+	export default {
+		// eslint-disable-next-line vue/multi-word-component-names
+		name: "Product",
+	};
+</script>
